001a8fb8db15d9aa9bbacd1e84d1929b10729d2a
Jasiek <jan.szymczak10@gmail.com>
2024-03-04 16:27:32 +0100

    Completed solver functionality. Added graphs. Started working on testing.

--- solver.py
+++ solver.py
@@ -8,14 +8,14 @@ class GradientIndent():
     def get_parameters(self):
         return self.beta
 
-    def solver(self, problem, gradient, epsilon, *args):
+    def solver(self, problem, gradient, epsilon, *args, show_data=True):
         it = 0
         point = np.array(args)
         if len(point) == 1:
             while abs((gradient(*point))) > epsilon:
                 point = point - self.beta * gradient(point)
                 it += 1
-            return [problem(*point), it]
+
         else:
             while not self.check_slope(problem, epsilon, point):
                 point, more_it = self.slope_direction(problem, epsilon, point)
@@ -23,7 +23,12 @@ class GradientIndent():
             while any(abs(value) > epsilon for value in gradient(*point)):
                 point = point - self.beta * np.array(gradient(*point))
                 it += 1
-            return [round(problem(*point),4), it]
+        if show_data:
+            print("--------------------")
+            print("Found minimum at: ", *point)
+            print("Minimum value: ", problem(*point))
+            print("Iterations: ", it)
+            print("--------------------")
 
     def check_slope(self, problem, epsilon, variables):
         point = np.array(variables)
@@ -48,17 +53,3 @@ class GradientIndent():
                 it += 1
                 next_point += 1
         return [next_point, it]
-
-
-f = lambda x: 2*x**2 + 3*x - 1
-f_gradient = lambda x: 4*x + 3
-g = lambda x1, x2: 1 - 0.6*np.exp(- x1**2 - x2**2 ) - 0.4*np.exp( - (x1+1.75)**2 - (x2-1)**2)
-g_gradient = lambda x1, x2: (1.2*x1*np.exp(- x1**2 - x2**2) + 0.8*(x1 + 1.75)*np.exp(-(x1 + 1.75)**2 - (x2-1)**2),
-                              1.2*x2*np.exp(- x1**2 - x2**2) + 0.8*(x2 - 1)*np.exp(-(x1 + 1.75)**2 - (x2-1)**2))
-
-a = GradientIndent(0.25)
-print(a.solver(f, f_gradient, 0.00001, 500000000000000000))
-print(a.solver(g, g_gradient, 0.00001, -3, -2))
-print(g(-5, -5))
-print(g(-4, -4))
-# print(a.check_slope(g, 0.00001, -1, -1))
