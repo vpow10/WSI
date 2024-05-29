a38c095058c658990c111df3e65dcfc2bfbd9993
Jasiek <jan.szymczak10@gmail.com>
2024-05-29 19:48:38 +0200

    Universal implementation

--- Q_learning/solver.py
+++ Q_learning/solver.py
@@ -1,74 +1,58 @@
 import numpy as np
-import gym
 import random
 
+
 class solver():
-    def __init__(self):
-        self.env = gym.make('Taxi-v3')
-        self.state_size = self.env.observation_space.n
-        self.action_size = self.env.action_space.n
+    def __init__(self, state_size: int, action_size: int):
+        self.state_size = state_size
+        self.action_size = action_size
         self.q_table = np.zeros((self.state_size, self.action_size))
 
-    def random_agent(self):
-        steps = 100
-        self.env.reset()
-        for _ in range(steps):
-            action = self.env.action_space.sample()
-            self.env.step(action)
-            self.env.render()
-        self.env.close()
-
-    def q_learning_agent(
-            self, learning_rate: float, discount_rate: float, epsilon: float, decay: float, episodes: int, max_steps: int):
+    def q_learning(
+            self, action_info: callable, reset_func: callable,
+            learning_rate: float, discount_rate: float, epsilon: float, decay: float, episodes: int, max_steps: int
+    ):
         all_rewards = []
         all_steps = []
         for episode in range(episodes):
-            state = self.env.reset()[0]
             done = False
             final_reward = 0
+            state = reset_func()[0]
             for i in range(max_steps):
                 # exploration - exploitation trade-off
                 if random.uniform(0, 1) < epsilon:
                     # explore
-                    action = self.env.action_space.sample()
+                    action = random.choice(range(self.action_size))
                 else:
                     # exploit
                     action = np.argmax(self.q_table[state, :])
 
-                new_state, reward, done, truncated, _ = self.env.step(action)
+                new_state, reward, done, _, _ = action_info(action)
 
                 # update Q-table
-                self.q_table[state, action] = self.q_table[state, action] + learning_rate * (reward + discount_rate * np.max(self.q_table[new_state, :]) - self.q_table[state, action])
+                self.q_table[state, action] = self.q_table[state, action] + learning_rate * (
+                            reward + discount_rate * np.max(self.q_table[new_state, :]) - self.q_table[state, action])
 
                 state = new_state
-                if done or truncated:
+                if done:
                     break
-                epsilon *= np.exp(-decay * episode)
                 final_reward += reward
+            epsilon *= np.exp(-decay * episode)
             all_rewards.append(final_reward)
             all_steps.append(i)
             print(f'Episode {episode} done')
         print(f"Training completed after {episode+1} episodes")
-        self.env.close()
         return all_rewards, all_steps
 
-    def play(self, max_steps=100):
-        self.env = gym.make('Taxi-v3', render_mode='human')
-        state = self.env.reset()[0]
-        done = False
+    def play(
+            self, reset_function: callable, action_info: callable, max_steps=100):
+        state = reset_function()[0]
         rewards = 0
         for _ in range(max_steps):
             action = np.argmax(self.q_table[state, :])
-            new_state, reward, done, truncated, _ = self.env.step(action)
+            state, reward, done, _, _ = action_info(action)
             rewards += reward
-            state = new_state
-            if done or truncated:
+            if done:
                 break
         print(f"Total rewards: {rewards}")
-        self.env.close()
         return rewards
-
-if __name__ == '__main__':
-    s = solver()
-    s.q_learning_agent(0.9, 0.8, 1.0, 0.005, 1000, 100)
-    s.play()
--- Q_learning/testing.py
+++ Q_learning/testing.py
@@ -1,15 +1,51 @@
 from solver import solver
 import matplotlib.pyplot as plt
 import numpy as np
+import gym
 
+
+def single_run():
+    env = gym.make('Taxi-v3')
+    env.reset()
+    state_size = env.observation_space.n
+    action_size = env.action_space.n
+    s = solver(state_size, action_size)
+    rewards, steps = s.q_learning(
+        action_info=env.step, reset_func=env.reset, learning_rate=0.9,
+        discount_rate=1, epsilon=1.0, decay=0.005, episodes=1000, max_steps=100
+    )
+    plt.figure(1)
+    episodes = np.arange(0, len(rewards))
+    plt.scatter(episodes, rewards, linewidths=0.1, edgecolors='black')
+    plt.xlabel('Episodes')
+    plt.ylabel('Rewards')
+    plt.title('Q-learning rewards')
+    plt.figure(2)
+    plt.scatter(episodes, steps, linewidths=0.1, edgecolors='black')
+    plt.xlabel('Episodes')
+    plt.ylabel('Steps')
+    plt.title('Q-learning steps')
+    plt.show()
+    env.close()
+    env2 = gym.make('Taxi-v3', render_mode='human')
+    s.play(
+        reset_function=env2.reset, action_info=env2.step, max_steps=100
+    )
 
 def test_learning_rate():
-    s = solver()
+    env = gym.make('Taxi-v3')
+    env.reset()
+    state_size = env.observation_space.n
+    action_size = env.action_space.n
+    s = solver(state_size, action_size)
     rewards = []
     steps = []
     learning_rates = np.arange(0.1, 2.1, 0.1)
     for learning_rate in learning_rates:
-        r, st = s.q_learning_agent(learning_rate=learning_rate, discount_rate=0.8, epsilon=1.0, decay=0.005, episodes=1000, max_steps=100)
+        r, st = s.q_learning(
+            action_info=env.step, reset_func=env.reset, learning_rate=learning_rate,
+            discount_rate=0.8, epsilon=1.0, decay=0.005, episodes=1000, max_steps=100
+        )
         rewards.append(np.mean(r))
         steps.append(np.mean(st))
     plt.figure(1)
@@ -23,14 +59,22 @@ def test_learning_rate():
     plt.ylabel('Mean of steps')
     plt.title('Q-learning steps')
     plt.show()
+    env.close()
 
 def test_discount_rate():
-    s = solver()
+    env = gym.make('Taxi-v3')
+    env.reset()
+    state_size = env.observation_space.n
+    action_size = env.action_space.n
+    s = solver(state_size, action_size)
     rewards = []
     steps = []
     discount_rates = np.arange(0.1, 2.1, 0.1)
     for discount_rate in discount_rates:
-        r, st = s.q_learning_agent(learning_rate=0.9, discount_rate=discount_rate, epsilon=1.0, decay=0.005, episodes=1000, max_steps=100)
+        r, st = s.q_learning(
+            action_info=env.step, reset_func=env.reset, learning_rate=0.9,
+            discount_rate=discount_rate, epsilon=1.0, decay=0.005, episodes=1000, max_steps=100
+        )
         rewards.append(np.mean(r))
         steps.append(np.mean(st))
     plt.figure(1)
@@ -44,14 +88,22 @@ def test_discount_rate():
     plt.ylabel('Mean of steps')
     plt.title('Q-learning steps')
     plt.show()
+    env.close()
 
 def test_episode():
-    s = solver()
+    env = gym.make('Taxi-v3')
+    env.reset()
+    state_size = env.observation_space.n
+    action_size = env.action_space.n
+    s = solver(state_size, action_size)
     rewards = []
     steps = []
     episodes = np.arange(100, 2100, 100)
     for episode in episodes:
-        r, st = s.q_learning_agent(learning_rate=0.9, discount_rate=0.8, epsilon=1.0, decay=0.005, episodes=episode, max_steps=100)
+        r, st = s.q_learning(
+            action_info=env.step, reset_func=env.reset, learning_rate=0.9,
+            discount_rate=0.8, epsilon=1.0, decay=0.005, episodes=episode, max_steps=100
+        )
         rewards.append(np.mean(r))
         steps.append(np.mean(st))
     plt.figure(1)
@@ -65,24 +117,7 @@ def test_episode():
     plt.ylabel('Mean of steps')
     plt.title('Q-learning steps')
     plt.show()
-
-def single_run():
-    s = solver()
-    rewards, steps = s.q_learning_agent(learning_rate=0.9, discount_rate=1, epsilon=1.0, decay=0.005, episodes=1000, max_steps=100)
-    plt.figure(1)
-    episodes = np.arange(0, len(rewards))
-    plt.scatter(episodes, rewards, linewidths=0.1, edgecolors='black')
-    plt.xlabel('Episodes')
-    plt.ylabel('Rewards')
-    plt.title('Q-learning rewards')
-    plt.figure(2)
-    plt.scatter(episodes, steps, linewidths=0.1, edgecolors='black')
-    plt.xlabel('Episodes')
-    plt.ylabel('Steps')
-    plt.title('Q-learning steps')
-    plt.show()
-    s.play()
-
+    env.close()
 
 if __name__=="__main__":
     test_learning_rate()
